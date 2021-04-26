93f974eb1e7b4fb9808eb97a2b050fb0403a3291
Victor Shijie Wu <vic_wu1@hotmail.com>
2021-04-25 22:02:44 -0400

    update website and responsiveness

--- main.js
+++ main.js
@@ -2,23 +2,19 @@ $(document).ready(function() {
     $('.menu-toggler').on('click', function() {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+        document.getElementById("html").classList.toggle('open');
     });
 
     $('.top-nav .nav-link').on('click', function() {
         $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
-    });
-
-    $('nav a[href*="#"]').on('click', function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 2000);
+        document.getElementById("html").classList.toggle('open');
     });
 
     $('#up').on('click', function() {
         $('html, body').animate({
             scrollTop: 0
-        }, 2000);
+        }, 1000);
     });
 
     AOS.init({
